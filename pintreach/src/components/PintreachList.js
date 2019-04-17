3233eabcd4018f4066c5ecbfe333502a82d417a5
nevets615 <surj316@yahoo.com>
2019-04-17 16:19:39 -0400

    delete function working

--- pintreach/src/components/PintreachList.js
+++ pintreach/src/components/PintreachList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Pintreach.css";
+
 function PintreachList(props) {
-  const { title, url, img } = props.article;
+  const { title, url, img, id } = props.article;
   return (
     <div className="article-wrap">
       <img src={img} alt="Article"/>
       <h3>{title}</h3>
       <a href={url}>Read</a>
-      <button onClick={props.deleteArticles}>Delete</button>
+      <p onClick={()=> props.deleteArticle(id)}>Delete</p>
     </div>
   );
 }
