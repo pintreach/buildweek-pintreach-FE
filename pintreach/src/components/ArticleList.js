e4f4cc943e791a7d0cc3b1e78f2787cbfeea417a
Brad Mortensen <b.mort86@gmail.com>
2019-04-18 16:08:01 -0600

    removes extra console.logs

--- pintreach/src/components/ArticleList.js
+++ pintreach/src/components/ArticleList.js
@@ -25,7 +25,6 @@ class ArticleList extends React.Component {
 				.then((res) => {
 					console.log(res.data);
 					this.setState({ articles: res.data });
-					console.log(this.state);
 				})
 				.catch((err) => {
 					console.log(err);
