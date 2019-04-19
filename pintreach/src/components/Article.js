5a71b27a1afe396bc1fdcfafaaa38335ee7539fe
Brad Mortensen <b.mort86@gmail.com>
2019-04-19 10:00:27 -0600

    updating card styles

--- pintreach/src/components/Article.js
+++ pintreach/src/components/Article.js
@@ -4,14 +4,12 @@ import './Pintreach.css';
 function Article(props) {
 	const { title, url, id, type } = props.article;
 	return (
-		<div>
 			<div className="article-wrap" key={id}>
-				<img src={type} alt="Article" />
+				<img className="card-img" src={type} alt="Article" />
 				<h3>{title}</h3>
 				<a href={url}>Read</a>
 				<p onClick={() => props.deleteArticle(id)}>Delete</p>
 			</div>
-		</div>
 	);
 }
 
